bfa2d26fd0a9eddc736678a6fed4285f5461e26b
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-02-14 17:34:09 -0300

    ex-04

---
+++ exercises/4_1/ex4.js
@@ -0,0 +1,13 @@
+const c = true
+switch(c){
+    case true:
+    console.log("positive")
+    break;
+
+    case false:
+    console.log("negative")
+    break;
+
+    default:
+    console.log("zero")
+}
