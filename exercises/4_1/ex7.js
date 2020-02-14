27e414038607373a941d337b96b9b0844377d271
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-02-14 18:18:21 -0300

    ex-07

---
+++ exercises/4_1/ex7.js
@@ -0,0 +1,16 @@
+const p=80
+if(p>=90 && p<=100) {
+    console.log("A");  
+}else if (p>= 80){
+    console.log("B");      
+}else if (p>= 70){
+    console.log("C");      
+}else if(p>= 60){
+    console.log("D");      
+}else if(p>= 50){
+    console.log("E");      
+}else if(p< 50){
+    console.log("F");      
+}else{
+    console.log("[ERRO]");
+}
