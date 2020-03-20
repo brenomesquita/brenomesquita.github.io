d4963d677fd27fc6619ac12132dc38e1a8506626
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-20 18:43:14 -0300

    teste

---
+++ exercises/8_3/2.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+function myIndexOf(arr, item) {
+  let searchedIndex = -1;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item === arr[i]) {
+      searchedIndex = i;
+    }
+  }
+  return searchedIndex;
+}
+
+
+// implemente seus testes aqui
+assert.equal(myIndexOf([1, 2, 3, 4], 3), [-1, -1, 3, -1], "erro")
+assert.equal(myIndexOf([1, 2, 3, 4], 5), [-1, -1, -1, -1], "erro")
