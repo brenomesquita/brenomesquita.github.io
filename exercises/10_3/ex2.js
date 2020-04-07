358a8d52a7cb14263928096218573463ee6e3641
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-07 00:39:24 -0300

    ok

--- exercises/10_3/ex2.js
+++ exercises/10_3/ex2.js
@@ -7,4 +7,8 @@ let red = () => arr.reduce((e,i)=>e+i,0)
 let chamar = red()
 let promisse = new Promise((resolve,reject) => chamar<8000 ? resolve('ok') : reject())
 
-promisse.then((suc) => console.log(`${chamar} ${suc}`)).catch(()=>console.log(`Deu ruim`))
+promisse.then((suc) => console.log(`${[2,3,5,10].map(n=>Math.floor(chamar/n))} ${suc}`))
+        .then(()=> console.log(chamar))
+        .catch(()=>console.log(`É mais de oito mil! Essa promise deve estar quebrada!`))
+
+
