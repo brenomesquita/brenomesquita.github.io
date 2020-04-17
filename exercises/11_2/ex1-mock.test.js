ca735bf428effcf0b8b34bf300600080171cc00d
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-17 00:11:07 -0300

    dormir

--- exercises/11_2/ex1-mock.test.js
+++ exercises/11_2/ex1-mock.test.js
@@ -1,30 +1,32 @@
-const numerpAleatorio = () => Math.floor(Math.random()*101)
+const mrequire= require('../11_2/ex1-mock')
 it('retorno 10', () =>{
-  const numerpAleatorio = jest.fn().mockReturnValue(10);
-  expect(numerpAleatorio()).toBe(10)
-  expect(numerpAleatorio).toHaveBeenCalled()
-  expect(numerpAleatorio).toHaveBeenCalledTimes(1)
+  mrequire.numerpAleatorio = jest.fn().mockReturnValue(10);
+  expect(mrequire.numerpAleatorio()).toBe(10)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalled()
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledTimes(1)
 })
 it('recebe dois param e retorna a divisao do primeiro pelo segundo', ()=>{
-  const numerpAleatorio = jest.fn().mockImplementationOnce((a, b) => a / b);
-  expect(numerpAleatorio(10,5)).toBe(2)
-  expect(numerpAleatorio).toHaveBeenCalled()
-  expect(numerpAleatorio).toHaveBeenCalledTimes(1)
-  expect(numerpAleatorio).toHaveBeenCalledWith(10, 5);
+  mrequire.numerpAleatorio = jest.fn().mockImplementationOnce((a, b) => a / b);
+  expect(mrequire.numerpAleatorio(10,5)).toBe(2)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalled()
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledTimes(1)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledWith(10, 5);
 })
 describe('ex 3', ()=>{
   it('recebe tres param de retorna e sua mult', ()=>{
-  const numerpAleatorio = jest.fn().mockImplementationOnce((a, b, c) => a * b * c);
-  expect(numerpAleatorio(1,2,3)).toBe(6)
-  expect(numerpAleatorio).toHaveBeenCalled()
-  expect(numerpAleatorio).toHaveBeenCalledTimes(1)
-  expect(numerpAleatorio).toHaveBeenCalledWith(1,2,3)
+  mrequire.numerpAleatorio = jest.fn().mockImplementationOnce((a, b, c) => a * b * c);
+  expect(mrequire.numerpAleatorio(1,2,3)).toBe(6)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalled()
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledTimes(1)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledWith(1,2,3)
 })
 it('reseta e criar uma nova que recebe um param e retorne o dobro', ()=>{
-  numerpAleatorio.mockReset()
-  //jest.fn().mockImplementationOnce((a) => a * a);
-  //expect(numerpAleatorio(2)).toBe(4)
-  //expect(numerpAleatorio).toHaveBeenCalled()
-  expect(numerpAleatorio).toHaveBeenCalledTimes(0)
+  mrequire.numerpAleatorio.mockReset()
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledTimes(0)
+  mrequire.numerpAleatorio.mockImplementationOnce((a) => a * a);
+  expect(mrequire.numerpAleatorio(2)).toBe(4)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalled()
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledTimes(1)
+  expect(mrequire.numerpAleatorio).toHaveBeenCalledWith(2)
 })
 })
